f943962a96e387e4aaa18f653c47dd54d1356f03
Alex Kloss <klossalex@gmail.com>
2013-03-25 21:13:55 -0400

    Implement card search
    Criteria limited to name for now, until support in the backend exists for the others

--- src/org/walkingarchive/app/SearchResultsActivity.java
+++ src/org/walkingarchive/app/SearchResultsActivity.java
@@ -1,12 +1,18 @@
 package org.walkingarchive.app;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class SearchResultsActivity extends Activity {
@@ -17,15 +23,45 @@ public class SearchResultsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_result);
         
+        String resultString = getIntent().getExtras().getString("resultString");
+        JSONArray json;
+		try
+		{
+			json = new JSONArray(resultString);
+		}
+		catch(JSONException e)
+		{
+			// Hmm...
+			return;
+		}
+		
         resultsListView = (ListView) findViewById(R.id.resultsListView);
+        ArrayList<String> listViewArray = new ArrayList<String>();
+        for(int i = 0; i < json.length(); i++)
+        {
+        	JSONObject cardJson;
+        	String name;
+        	try
+        	{
+				cardJson = json.getJSONObject(i);
+				name = cardJson.getString("name");
+			}
+        	catch (JSONException e)
+        	{
+        		// TODO: Log/whatever this
+        		continue;
+			}
+        	listViewArray.add(name);
+        }
+        
+        // Use some arbitrary TextView as a template
+        resultsListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listViewArray));
+        
         resultsListView.setOnItemClickListener(
 			new OnItemClickListener()
 			{
 				public void onItemClick(AdapterView<?> adapter, View view, int position, long id)
 				{
-					// TODO: Use Intent.getStringArrayListExtra to retrieve the search results
-			        // and add them to the list view
-					
 					Intent cardViewerIntent = new Intent(SearchResultsActivity.this, CardViewerActivity.class);
 			    	SearchResultsActivity.this.startActivity(cardViewerIntent);
 				}
