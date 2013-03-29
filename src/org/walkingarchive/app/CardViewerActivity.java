4dd931c948661abcfe613355fd0237c35087d21f
lxl377 <lxl377@case.edu>
2013-03-29 15:51:20 -0400

    UI new

--- src/org/walkingarchive/app/CardViewerActivity.java
+++ src/org/walkingarchive/app/CardViewerActivity.java
@@ -1,13 +1,7 @@
 package org.walkingarchive.app;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class CardViewerActivity extends Activity {
 	@Override
@@ -17,22 +11,5 @@ public class CardViewerActivity extends Activity {
         
         // TODO: Use Intent.getStringExtra() to pull the card's properties and
         // set the Views' text properties appropriately
-        
-        final Context context = this;
-		Button gobackButton = (Button) findViewById(R.id.gobackButton);
-		
-		gobackButton.setOnClickListener(new OnClickListener() {
-			 
-			@Override
-			public void onClick(View arg0) {
- 
-			    Intent intent = new Intent(context, SearchActivity.class);
-                            startActivity(intent);   
- 
-			}
- 
-		});
-		
-        
     }
 }
