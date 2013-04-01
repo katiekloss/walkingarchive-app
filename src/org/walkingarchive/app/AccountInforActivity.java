96d3d242e8efe584a344ede7d26427b564ef92bd
Alex Kloss <klossalex@gmail.com>
2013-04-01 16:44:24 -0400

    Clean up unused imports and XML warnings

--- src/org/walkingarchive/app/AccountInforActivity.java
+++ src/org/walkingarchive/app/AccountInforActivity.java
@@ -7,14 +7,9 @@ import org.walkingarchive.app.ui.SearchResult;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class AccountInforActivity extends Activity {
 	JSONArray json;
