9684ade137f40846fa91dfb82db50ff53fe3b059
Katie Kloss <kloss.katherine@gmail.com>
2013-04-14 15:47:13 -0400

    Add prebuilt tesseract-android-tools and PoC code

--- src/org/walkingarchive/app/ocr/OCR.java
+++ src/org/walkingarchive/app/ocr/OCR.java
@@ -1,5 +1,7 @@
 package org.walkingarchive.app.ocr;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
 import android.graphics.Bitmap;
 
 public class OCR {
@@ -8,6 +10,12 @@ public class OCR {
 	
 	public String runOCR(Bitmap image)
 	{
-		return "Defender (This creature can't attack.) Walking Archive comes into play with a +1/+1 counter on it. At the beginning of each player's upkeep, that player draws a card for each +1/+1 counter on Walking Archive. : Put a +1/+1 counter on Walking Archive"; 
+		TessBaseAPI tesseract = new TessBaseAPI();
+		tesseract.init("/mnt/sdcard/tesseract/", "eng");
+		tesseract.setImage(image);
+		String text = tesseract.getUTF8Text();
+		tesseract.end();
+		return text;
+		//return "Defender (This creature can't attack.) Walking Archive comes into play with a +1/+1 counter on it. At the beginning of each player's upkeep, that player draws a card for each +1/+1 counter on Walking Archive. : Put a +1/+1 counter on Walking Archive"; 
 	}
 }
