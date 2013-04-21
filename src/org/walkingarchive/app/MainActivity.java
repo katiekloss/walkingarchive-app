d4f5c7133d6cf0f9988efd8c7117906669779d27
Katie Kloss <kloss.katherine@gmail.com>
2013-04-21 17:04:53 -0400

    Add custom Magic wordlist for Tesseract

--- src/org/walkingarchive/app/MainActivity.java
+++ src/org/walkingarchive/app/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         copyAsset("eng.traineddata", "tessdata");
+        copyAsset("eng.user-words", "tessdata");
     }
 
     public void copyAsset(String filename, String destDir)
