85ee5e989847267b1dbd9bbe58877688a53c155a
William Earley <wae22@case.edu>
2013-04-23 09:18:44 -0400

    added  ability to look at the picture or text

--- src/org/walkingarchive/app/ImageActivity.java
+++ src/org/walkingarchive/app/ImageActivity.java
@@ -14,7 +14,6 @@ import android.widget.Spinner;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.widget.Button;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -22,32 +21,83 @@ import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.googlecode.leptonica.android.Pix;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import bin.classes.*;
+import bin.classes.com.googlecode.leptonica.android.ReadFile;
 
 public class ImageActivity extends Activity {
 
+	public static File file;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 	    Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    
-	    File file = new File(Environment.getExternalStorageDirectory(), "card.jpg");
+	    file = new File(Environment.getExternalStorageDirectory(), "card.jpg");
 	    
 	    Uri outputFileUri = Uri.fromFile(file);
         pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-        startActivityForResult(pictureIntent, 1);        
+        startActivityForResult(pictureIntent, 1);     
+     
+
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == 1){
+				if (requestCode == 1){
 			Uri imageUri = null;
 			if (data!=null) {
 				if (data.hasExtra("data")) {
 					Bitmap thumbnail = data.getParcelableExtra("data");
 				}	
 			} else {
-				//Call Tesseract on the Image here
+				
 			}
+			
+			
+			//call tesseract
+			
+			//This is using the tess-two library
+			//Pix pix = ReadFile.readFile(file);
+			
+			
+			
+			//This should work, but it gives an error 
+			/*
+			File myDir = getExternalFilesDir(Environment.MEDIA_MOUNTED);
+
+			TessBaseAPI baseApi = new TessBaseAPI();
+			baseApi.init(myDir.toString(), "eng"); // myDir + "/tessdata/eng.traineddata" must be present
+			baseApi.setImage(file);
+
+			String recognizedText = baseApi.getUTF8Text(); // Log or otherwise display this string...
+			baseApi.end();
+			*/
+			
+			//save file to enviornment
+			
+			
+			//intent to show output of text file
+		/*	Intent intent = new Intent();
+			intent.setAction(android.content.Intent.ACTION_VIEW);
+			intent.setDataAndType(Uri.fromFile(file),"text/*");
+			startActivity(intent);
+			*/
+			
+			
+			
+			//intent to show output of picture
+			/*
+			Intent intent = new Intent();
+			intent.setAction(android.content.Intent.ACTION_VIEW);
+			intent.setDataAndType(Uri.fromFile(file),"image/*");
+			startActivity(intent);
+			*/
+					
 		}        
 	}
 
